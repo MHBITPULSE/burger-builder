0d07cda7f430399d0d18c026c26c2219c027cc27
MHBITPULSE <100596860+MHBITPULSE@users.noreply.github.com>
2023-09-08 18:27:29 +0600

    Controls Added

--- burger-frontend/src/components/BurgerBuilder/BurgerBuilder.jsx
+++ burger-frontend/src/components/BurgerBuilder/BurgerBuilder.jsx
@@ -5,14 +5,37 @@ import Controls from './Controls/Controls'
 const BurgerBuilder = () => {
       const [ingredients, setIngredients] = useState(
             [
-                  { type: "salad", amount: 0 },
-                  { type: "cheese", amount: 0 },
-                  { type: "meat", amount: 0 },
+                  { type: "salad", amount: 0, unitPrice: 20 },
+                  { type: "cheese", amount: 0, unitPrice: 40 },
+                  { type: "meat", amount: 0, unitPrice: 90 },
             ])
+      const [totalPrice, setTotalPrice] = useState(80)
+      const addItem = (type) => {
+            let ingredient = [...ingredients]
+            for (let item of ingredient) {
+                  if (item.type === type) {
+                        item.amount++
+                        setTotalPrice(totalPrice + item.unitPrice)
+                  }
+            }
+            setIngredients(ingredient)
+      }
+
+      const removeItem = (type) => {
+            let ingredient = [...ingredients]
+            for (let item of ingredient) {
+                  if ((item.type === type)) {
+                        if (item.amount === 0) return;
+                        item.amount--
+                        setTotalPrice(totalPrice - item.unitPrice)
+                  }
+            }
+            setIngredients(ingredient)
+      }
       return (
-            <div className='flex flex-col'>
+            <div className='flex sm:flex-col flex-row'>
                   <Burger ingredients={ingredients} />
-                  <Controls />
+                  <Controls addItem={addItem} removeItem={removeItem} totalPrice={totalPrice} />
             </div>
       )
 }
--- burger-frontend/src/components/BurgerBuilder/Controls/Controls.jsx
+++ burger-frontend/src/components/BurgerBuilder/Controls/Controls.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { Card, CardBody, CardFooter, CardHeader } from 'reactstrap'
 
-const Controls = () => {
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const BuildControl = ({ label, type, addItem, removeItem }) => {
     return (
-        <div>
-            <Card>
-                <CardHeader>
+        <div className='flex'>
+            <div className='mr-auto ml-5 font-bold text-xl'>{label}</div>
+            <button className='btn btn-danger btn-sm m-1' onClick={() => removeItem(type)}>Less</button>
+            <button className='btn btn-success  btn-sm m-1' onClick={() => addItem(type)}>More</button>
+        </div>
+    )
+}
+
+const Controls = ({ addItem, removeItem, totalPrice }) => {
+    return (
+        <div className='container ml-md-5 text-center'>
+            <Card className='mt-6 mb-6 text-center'>
+                <CardHeader className='text-white bg-black'>
                     <h4>Add Ingredient</h4>
                 </CardHeader>
                 <CardBody>
-
+                    {controls.map((item, index) => {
+                        return <BuildControl label={item.label} type={item.type} key={index} addItem={addItem} removeItem={removeItem} />
+                    })}
                 </CardBody>
-                <CardFooter><h5>Price:  BDT</h5></CardFooter>
+                <CardFooter><h5>Price:  {totalPrice} BDT</h5></CardFooter>
             </Card>
         </div>
     )
